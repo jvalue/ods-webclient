cd5b231202688a816a6060ad1c700f9f1dac9212
Artur Wasinger <art.wasinger@gmail.com>
2018-07-03 06:42:01 +0200

    Minor changes to app.component.html and app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Route, RouterModule} from '@angular/router';
+import {Route, RouterModule, Routes} from '@angular/router';
+import {AppComponent} from './app.component';
+import {LayoutComponent} from './layout/layout.component';
 
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'dashboard', component: AppComponent },
+  { path: '**', component: AppComponent }
 
+];
 @NgModule({
   exports: [RouterModule]
 })
