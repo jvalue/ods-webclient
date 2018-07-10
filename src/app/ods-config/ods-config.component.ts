597c10e2b2225029c7e0ced3f61f3dbfe807b5cc
Artur Wasinger <art.wasinger@gmail.com>
2018-07-10 07:21:17 +0200

    Deleted empty files and removed dependencies

--- src/app/ods-config/ods-config.component.ts
+++ src/app/ods-config/ods-config.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ods-config',
   templateUrl: './ods-config.component.html',
-  styleUrls: ['./ods-config.component.css']
 })
 export class OdsConfigComponent implements OnInit {
 
--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
 
