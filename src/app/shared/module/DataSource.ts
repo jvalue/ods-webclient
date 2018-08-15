f22449dbb9081cb2494e703ebe509e28f89dbff7
Artur Wasinger <art.wasinger@gmail.com>
2018-08-15 19:54:20 +0200

    Added DataSource.ts where requests from datasource-service are handled

--- src/app/shared/module/DataSource.ts
+++ src/app/shared/module/DataSource.ts
@@ -2,14 +2,101 @@
 Copyright (c) 2018 Friedrich-Alexander University Erlangen-Nürnberg (FAU)
 SPDX-License-Identifier: AGPL-3.0-only
 */
+import {DataSourceService} from '../services/data-source.service';
+import {Injectable} from '@angular/core';
+import {SettingsService} from '../services/settings.service';
+
+
+@Injectable({
+  providedIn: 'root',
+})
 export class DataSource {
-  domainIdKey: number;
-  schema: Array<string>;
-  name: string;
-  title: string;
-  author: string;
-  authorEmail: string;
-  notes: string;
-  url: string;
-  termsOfUse: string;
+
+  headers: string[];
+  bodyObject: any[];
+  public JSON = JSON;
+  public resp: any[];
+
+  constructor(private dataService: DataSourceService,
+              private settingsSevice: SettingsService) {}
+
+  public getText(){
+    console.log(this.dataService.odsBaseUrl);
+    console.log("Basic" + btoa(
+      this.settingsSevice.getUsername() +":"+ this.settingsSevice.getPassword()));
+  }
+
+
+  public createBodyForDataSource(): Object {
+    let body = {
+      domainIdKey: "/id",
+      schema: {},
+      metaData: {
+        name: "TestData",
+        title: "TestData",
+        author: "",
+        authorEmail: "",
+        notes: "",
+        url: "",
+        termsOfUse: ""
+      }
+    };
+  return body;
+ }
+
+  public async getDataSource() {
+    try {
+      const data = await this.dataService.getDataSource().toPromise();
+      const keys = data.headers.keys();
+      this.headers = keys.map(key => `${key}: ${data.headers.get(key)}`);
+      this.bodyObject = Object.values({...data.body});
+    } catch (e) {
+      console.error(e);
+    }
+
+  }
+
+  public async getDataSourceById(sourceId: String) {
+    try{
+    const data = await this.dataService.getDataSourceById(sourceId).toPromise();
+      const keys = data.headers.keys();
+      this.headers = keys.map(key => `${key}: ${data.headers.get(key)}`);
+      this.bodyObject = Object.values({...data.body});
+    }catch(e) {
+      console.log("error occured")
+      console.error(e);
+    }
+  }
+
+  public async getDataSourceSchemaById(sourceId: String){
+    try {
+      const data = await this.dataService.getDataSourceSchemaById(sourceId).toPromise();
+      const keys = data.headers.keys();
+      this.headers = keys.map(key => `${key}: ${data.headers.get(key)}`);
+      this.bodyObject = Object.values({...data.body});
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  public async addDataSource(sourceId: String) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append("Authorization", "Basic " + btoa(
+        this.settingsSevice.getUsername() + ":" + this.settingsSevice.getPassword()));
+
+    await this.dataService.addDataSource(sourceId, this.createBodyForDataSource(), {headers: headers}).toPromise()
+      .catch(error => console.error(error));
+  }
+
+  public async deleteDataSource(sourceId: String) {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append("Authorization", "Basic " + btoa(this.settingsSevice.getUsername() +
+      ":" + this.settingsSevice.getPassword()));
+
+    await this.dataService.deleteDataSource(sourceId, {headers: headers}).toPromise()
+      .catch(error => console.error(error));
+
+  }
 }
---
+++ src/app/shared/services/settings.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SettingsService {
+  odsBaseUrl = "http://localhost:8080/ods/api/v1/datasources";
+  versionUrl =  "http://localhost:8080/ods/api/v1";
+  public odsBaseName = "admin@adminland.com"; // "admin"
+  public odsBasePassword = "admin123";
+
+constructor() {}
+
+  public getBaseUrl(): String {
+    return this.odsBaseUrl;
+  }
+
+  public setBaseUrl(url: string){
+    this.odsBaseUrl = url;
+  }
+
+  public getUsername(): String {
+    return this.odsBaseName;
+  }
+
+  public getPassword(): String {
+    return this.odsBasePassword;
+  }
+
+}
