3bda47219274256c57292cdcb0a340921de9e719
Artur Wasinger <art.wasinger@gmail.com>
2018-08-15 13:02:09 +0200

    Data Service where the httpRequests are sent to ODS system

--- src/app/shared/services/data-source.service.ts
+++ src/app/shared/services/data-source.service.ts
@@ -3,17 +3,34 @@ Copyright (c) 2018 Friedrich-Alexander University Erlangen-Nürnberg (FAU)
 SPDX-License-Identifier: AGPL-3.0-only
 */
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {DataSource} from '../module/DataSource';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataSourceService {
 
+  odsBaseUrl = "http://localhost:8080/ods/api/v1/datasources";
+
   constructor(private http: HttpClient) { }
 
-  getDataSource() {
-    return this.http.get<DataSource>('');
+  getDataSource(): Observable<HttpResponse<Object>> {
+    return this.http.get(this.odsBaseUrl, {observe: 'response'});
+  }
+
+  getDataSourceById(sourceId: String): Observable<HttpResponse<Object>> {
+    return this.http.get(this.odsBaseUrl+'/' + sourceId, {observe: 'response'});
+  }
+
+  getDataSourceSchemaById(sourceId: String): Observable<HttpResponse<Object>> {
+    return this.http.get(this.odsBaseUrl +'/' + sourceId + '/schema', {observe: 'response'})
+  }
+
+  addDataSource(sourceId: String, body: any, options: any): Observable<any> {
+    return this.http.put(this.odsBaseUrl+'/' + sourceId, body, options);
+  }
+  deleteDataSource(sourceId: String, options: any): Observable<any> {
+    return this.http.delete(this.odsBaseUrl +'/' + sourceId, options);
   }
 }
