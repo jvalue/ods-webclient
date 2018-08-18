f01fdea0744d64f3695f904a5e3b464c1ad10809
Artur Wasinger <art.wasinger@gmail.com>
2018-08-18 14:45:41 +0200

    Adjusted data-source-service to basic-rest-service

--- src/app/shared/services/data-source.service.ts
+++ src/app/shared/services/data-source.service.ts
@@ -3,8 +3,9 @@ Copyright (c) 2018 Friedrich-Alexander University Erlangen-Nürnberg (FAU)
 SPDX-License-Identifier: AGPL-3.0-only
 */
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpResponse} from '@angular/common/http';
+import {HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {BasicRestService} from './basic-rest.service';
 
 @Injectable({
   providedIn: 'root',
@@ -13,24 +14,24 @@ export class DataSourceService {
 
   odsBaseUrl = "http://localhost:8080/ods/api/v1/datasources";
 
-  constructor(private http: HttpClient) { }
+  constructor(private service: BasicRestService) { }
 
   getDataSource(): Observable<HttpResponse<Object>> {
-    return this.http.get(this.odsBaseUrl, {observe: 'response'});
+    return this.service.get(this.odsBaseUrl);
   }
 
   getDataSourceById(sourceId: String): Observable<HttpResponse<Object>> {
-    return this.http.get(this.odsBaseUrl+'/' + sourceId, {observe: 'response'});
+    return this.service.get(this.odsBaseUrl+'/' + sourceId);
   }
 
   getDataSourceSchemaById(sourceId: String): Observable<HttpResponse<Object>> {
-    return this.http.get(this.odsBaseUrl +'/' + sourceId + '/schema', {observe: 'response'})
+    return this.service.get(this.odsBaseUrl +'/' + sourceId + '/schema')
   }
 
-  addDataSource(sourceId: String, body: any, options: any): Observable<any> {
-    return this.http.put(this.odsBaseUrl+'/' + sourceId, body, options);
+  addDataSource(sourceId: String, body: any): Observable<any> {
+    return this.service.put(this.odsBaseUrl+'/' + sourceId, body);
   }
-  deleteDataSource(sourceId: String, options: any): Observable<any> {
-    return this.http.delete(this.odsBaseUrl +'/' + sourceId, options);
+  deleteDataSource(sourceId: String, body: any): Observable<any> {
+    return this.service.delete(this.odsBaseUrl +'/' + sourceId, body);
   }
 }
