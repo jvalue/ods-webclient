eba3a71c9a6b874899ee000cb34f9b5001ac7ec3
Artur Wasinger <art.wasinger@gmail.com>
2018-08-18 14:54:03 +0200

    Adjusted data-source.service to dataSource model

--- src/app/shared/services/data-source.service.ts
+++ src/app/shared/services/data-source.service.ts
@@ -3,9 +3,9 @@ Copyright (c) 2018 Friedrich-Alexander University Erlangen-Nürnberg (FAU)
 SPDX-License-Identifier: AGPL-3.0-only
 */
 import { Injectable } from '@angular/core';
-import {HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {BasicRestService} from './basic-rest.service';
+import {DataSource} from '../model/dataSource';
 
 @Injectable({
   providedIn: 'root',
@@ -16,22 +16,22 @@ export class DataSourceService {
 
   constructor(private service: BasicRestService) { }
 
-  getDataSource(): Observable<HttpResponse<Object>> {
+  getDataSource(): Observable<DataSource[]> {
     return this.service.get(this.odsBaseUrl);
   }
 
-  getDataSourceById(sourceId: String): Observable<HttpResponse<Object>> {
+  getDataSourceById(sourceId: String): Observable<DataSource> {
     return this.service.get(this.odsBaseUrl+'/' + sourceId);
   }
 
-  getDataSourceSchemaById(sourceId: String): Observable<HttpResponse<Object>> {
+  getDataSourceSchemaById(sourceId: String): Observable<DataSource> {
     return this.service.get(this.odsBaseUrl +'/' + sourceId + '/schema')
   }
 
-  addDataSource(sourceId: String, body: any): Observable<any> {
+  addDataSource(sourceId: String, body: any): Observable<DataSource> {
     return this.service.put(this.odsBaseUrl+'/' + sourceId, body);
   }
-  deleteDataSource(sourceId: String, body: any): Observable<any> {
+  deleteDataSource(sourceId: String, body: any): Observable<DataSource> {
     return this.service.delete(this.odsBaseUrl +'/' + sourceId, body);
   }
 }
