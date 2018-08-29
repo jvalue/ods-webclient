77afbf629c46607ac90c0c59f66f48e6339dec83
Artur Wasinger <art.wasinger@gmail.com>
2018-08-29 21:05:11 +0200

    Commented faulty skeleton to be able to compile

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {User} from '../model/user';
+// import {User} from '../model/user';
 
 @Injectable({
   providedIn: 'root'
@@ -8,20 +8,20 @@ export class UserService {
 
   constructor() { }
 
-  getAllUsers(): User[] {
-    return null;
-  }
-
-  addUser(userName: string, password: string) {}
-
-  getUserById(userId: string): User {
-    return null;
-  }
-
-  getUserMe(): User {
-    return null;
-  }
-
-  removeUser(userId: string) {}
+  // getAllUsers(): User[] {
+  //   return null;
+  // }
+  //
+  // addUser(userName: string, password: string) {}
+  //
+  // getUserById(userId: string): User {
+  //   return null;
+  // }
+  //
+  // getUserMe(): User {
+  //   return null;
+  // }
+  //
+  // removeUser(userId: string) {}
 
 }
