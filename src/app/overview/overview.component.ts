46a802b26190772a81b144fad7ccecd2d6517be9
Artur Wasinger <art.wasinger@gmail.com>
2018-09-12 18:40:57 +0200

    Modified overview component to display user data

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -6,19 +6,26 @@ import { Component, OnInit } from '@angular/core';
 import {User} from '../shared/model/user';
 import {UserService} from '../shared/services/user.service';
 import {Observable} from 'rxjs';
+import {DataSourceService} from '../shared/services/data-source.service';
+import {DataSource} from '../shared/model/dataSource';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.css']
 })
 export class OverviewComponent implements OnInit {
   public users: Observable<User[]>;
+  public datasources: Observable<DataSource[]>;
   public myself: Observable<User>;
+  displayedColumns: string[] = ['email', 'name', 'role', 'id'];
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService,
+              private  dataSourceService: DataSourceService) {}
 
   ngOnInit() {
     this.users = this.userService.getAllUsers();
+    this.datasources = this.dataSourceService.getDataSource();
   }
   getMyself() {
     this.myself = this.userService.getMyself();
