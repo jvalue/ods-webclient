f6524d1805ed6faa4151c453216b61fd7a2b852c
Artur Wasinger <art.wasinger@gmail.com>
2018-10-24 09:56:57 +0200

    Added dynamic argument form inputs for adapters

--- src/app/ods-config/components/processor-chain-form/processor-chain-form.component.ts
+++ src/app/ods-config/components/processor-chain-form/processor-chain-form.component.ts
@@ -1,19 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {ProcessorSpecification} from '../../../shared/model/processor-specification';
-import {Observable} from 'rxjs';
 import {ProcessorSpecificationService} from '../../../shared/services/processor-specification.service';
 import {map} from 'rxjs/operators';
-import {KeyValue} from '@angular/common';
+import {isNullOrUndefined} from 'util';
 
 @Component({
   selector: 'app-processor-chain-form',
   templateUrl: './processor-chain-form.component.html',
 })
 export class ProcessorChainFormComponent implements OnInit {
-
-  obj: Observable<ProcessorSpecification[]>;
-  Object: ObjectConstructor;
 
   timeUnits = ['MINUTES', 'SECONDS', 'MILISECONDS', 'HOURS'];
 
@@ -22,46 +18,60 @@ export class ProcessorChainFormComponent implements OnInit {
 
   filterFormGroup: FormGroup;
   filterFormArray: FormArray;
-  filterArray: Observable<ProcessorSpecification[]>;
+  filterArray: ProcessorSpecification[] = [];
+  currentFilter: any;
+  filterConfigOptions: string[];
+  filterConfigFormArray: FormArray;
 
   adapterFormGroup: FormGroup;
-  adapterArray: Observable<ProcessorSpecification[]>;
-  adapterArgumentsArray: Observable<KeyValue<string, any>>;
+  adapterArray: ProcessorSpecification[] = [];
+  currentAdapter: any;
+  adapterConfigFormArray: FormArray;
+  adapterConfigOptions: string[];
 
   exIntervalBool: Boolean = false;
   executionIntervalForm: FormGroup;
+
+
+
 
   constructor(private _formBuilder: FormBuilder,
               private processorSpecService: ProcessorSpecificationService) {
   }
 
   ngOnInit() {
-    this.filterArray = this.processorSpecService.getAllProcessorSpecifications().pipe(map(
+    this.processorSpecService.getAllProcessorSpecifications().pipe(map(
       data => this.filterByType(data, 'FILTER')
-    ));
-    this.adapterArray = this.processorSpecService.getAllProcessorSpecifications().pipe(map(
+    )).subscribe(value => {
+      this.filterArray = value;
+    });
+
+    this.processorSpecService.getAllProcessorSpecifications().pipe(map(
       data => this.filterByType(data, 'SOURCE_ADAPTER')
-    ));
+    )).subscribe(value => {
+      this.adapterArray = value;
+    });
+
+    this.adapterConfigFormArray = this._formBuilder.array([]);
     this.adapterFormGroup = this.createProcessor();
-    this.filterFormArray = this._formBuilder.array([this.createProcessor()]);
+
+    this.filterConfigFormArray = this._formBuilder.array([]);
+    this.filterFormArray = this._formBuilder.array([this.createFilter()]);
     this.filterFormGroup = this._formBuilder.group({
       filters: this.filterFormArray
     });
     this.processorArray = new FormArray([]);
-    this.executionIntervalForm = new FormGroup({});
     this.processorChainForm = this._formBuilder.group({
       id: ['', Validators.required],
       processors: this.processorArray,
       executionInterval: null
     });
+    this.executionIntervalForm = new FormGroup({});
     this.setExecutionInterval();
-
-
-    this.obj = this.processorSpecService.getAllProcessorSpecifications();
   }
 
   filterByType(data: any, type: string) {
-    const arr = new Array();
+    const arr = [];
     for (let i = 0; i < data.length; i++) {
       if (data[i].type === type) {
         arr.push(data[i]);
@@ -71,53 +81,88 @@ export class ProcessorChainFormComponent implements OnInit {
   }
 
   setExecutionInterval() {
-    this.executionIntervalForm.addControl('period', new FormControl('', [
-      Validators.min(1), Validators.max(60), Validators.pattern('[0-9]*') ]
-      ));
+    this.executionIntervalForm.addControl('period', new FormControl('1', [
+      Validators.min(1), Validators.max(60), Validators.pattern('[0-9]*')]
+    ));
     this.executionIntervalForm.addControl('unit', new FormControl());
   }
 
   createProcessor() {
     return this._formBuilder.group({
       name: [''],
-      arguments: this._formBuilder.group({})
+      arguments: this.adapterConfigFormArray,
+    });
+  }
+
+  createFilter() {
+    return this._formBuilder.group({
+      name: [''],
+      arguments: this.filterConfigFormArray,
     });
   }
 
   addFilter() {
-    this.filterFormArray.push(this.createProcessor());
+    this.filterFormArray.push(this.createFilter());
   }
 
   removeFilter(index: number) {
     this.filterFormArray.removeAt(index);
+    while (this.filterConfigFormArray.length > 0) {
+      this.filterConfigFormArray.removeAt(0);
+    }
   }
 
-  // getArgumentsFromAdapter(name: string) {
-  //   const args = new Array<KeyValue<string, string>>();
-  //   this.adapterArray.subscribe(
-  //     data => {
-  //       const o = data[index].argumentTypes;
-  //       const keys = Object.keys(o);
-  //       const values = Object.values(o);
-  //     }
-  //   );
-  // }
-  // getArgumetnsFromFilter(name: string) {
-  //
-  // }
+  updateAdapterSettings(adapterName: string) {
 
-  test() {
-    this.obj.subscribe(data => {
-      for (let i = 0; i < data.length; i++) {
-        console.log('from ' + i);
-        const o = data[i].argumentTypes;
-        Object.keys(o).map(e => console.log(e));
-        Object.values(o).map(e => console.log(e));
-
+    let adapter = null;
+    for (const adap of this.adapterArray) {
+      if (adap['name'] === adapterName) {
+        adapter = adap;
       }
-    });
+    }
+    if (isNullOrUndefined(adapter)) {
+      throw Error('Failed to locate the adapter in the adapterArray.');
+    }
+
+    this.currentAdapter = adapter;
+    this.adapterConfigOptions = Object.keys(adapter.argumentTypes);
+
+    while (this.adapterConfigFormArray.length > 0) {
+      this.adapterConfigFormArray.removeAt(0);
+    }
+
+    for (const type of this.adapterConfigOptions) {
+      const formOptions: any = {};
+      formOptions[type] = [''];
+      this.adapterConfigFormArray.push(this._formBuilder.group(formOptions));
+    }
+
   }
 
+  updateFilterArguments(filterName: string) {
+    let filter = null;
+    for (const filt of this.filterArray) {
+      if (filt['name'] === filterName) {
+        filter = filt;
+      }
+    }
+    if (isNullOrUndefined(filter)) {
+      throw Error('Failed to locate the adapter in the adapterArray.');
+    }
+    this.currentFilter = filter;
+    this.filterConfigOptions = Object.keys(filter.argumentTypes);
+    console.log(this.filterConfigOptions);
+
+    // while (this.adapterConfigFormArray.length > 0) {
+    //   this.adapterConfigFormArray.removeAt(0);
+    // }
+    //
+    // for (const type of this.filterConfigOptions) {
+    //   const formOptions: any = {};
+    //   formOptions[type] = [''];
+    //   this.filterConfigFormArray.push(this._formBuilder.group(formOptions));
+    // }
+  }
 
   save() {
     if (this.processorChainForm.valid) {
@@ -135,5 +180,6 @@ export class ProcessorChainFormComponent implements OnInit {
     }
     console.log(this.processorChainForm.getRawValue());
   }
+
 
 }
