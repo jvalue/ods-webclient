a0aafb8450c9f706b4f66920371133c3c7449f1d
Andreas Bauer <dev@andreasbauer.org>
2019-02-06 20:18:52 +0100

    Add dependency for material design icons

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import App from './App.vue';
 import router from './router';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import '@mdi/font/css/materialdesignicons.css';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  iconfont: 'mdi',
+});
 
 Vue.config.productionTip = false;
 
