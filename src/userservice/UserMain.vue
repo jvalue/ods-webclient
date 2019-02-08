58b8e6cd477c5edf02087b47b81c8843a91270cf
Mathias Zinnen <mathias.zinnen@gmail.com>
2019-02-08 14:36:44 +0000

    wip: fetch users from backend

--- src/userservice/UserMain.vue
+++ src/userservice/UserMain.vue
@@ -2,16 +2,20 @@
   <div class="user-main">
     <h1>User-Service Configuration</h1>
     <h2>Backend version: {{version}}</h2>
+    <p>users: {{users}}</p>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import * as UserRestService from './userRest';
+import User from './user';
 
 @Component
 export default class UserMain extends Vue {
   public version: string = '2.0.0';
+  public users: User[] = UserRestService.getAllUsers();
 }
 </script>
 
