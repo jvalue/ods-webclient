56ecf01cc4baadc6bd1ae60ababd2f9ccd8f10cf
Mathias Zinnen <mathias.zinnen@gmail.com>
2019-02-13 21:13:19 +0000

    wip: user table

--- src/userservice/UserMain.vue
+++ src/userservice/UserMain.vue
@@ -2,9 +2,18 @@
   <div class="user-main">
     <h1>User-Service Configuration</h1>
     <h2>Backend version: {{version}}</h2>
-    <ul>
-      <li v-for="user in users" :key="user.id">{{user}}</li>
-    </ul>
+    <v-data-table :headers="headers" :items="users" class="elevation-1">
+      <template slot="items" slot-scope="props">
+        <td class="text-xs-left">{{props.item.id}}</td>
+        <td class="text-xs-left">{{ props.item.name}}</td>
+        <td class="text-xs-left">{{props.item.email}}</td>
+        <td class="text-xs-left">{{props.item.role}}</td>
+        <td class="justify-center layout px-0">
+          <v-icon small class="mr-2" @click="editUser(props.item)">edit</v-icon>
+          <v-icon small @click="deleteUser(props.item)">delete</v-icon>
+        </td>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
@@ -18,6 +27,12 @@ import User from './user';
 export default class UserMain extends Vue {
   public version: string = '2.0.0';
   public users: User[] = [];
+  public headers = [
+    { text: 'id', value: 'id' },
+    { text: 'name', value: 'name' },
+    { text: 'email', value: 'email' },
+    { text: 'role', value: 'role' },
+  ];
 
   private mounted() {
     this.getUsers();
@@ -27,6 +42,14 @@ export default class UserMain extends Vue {
     UserRestService.getAllUsers().then(r => {
       this.users.push(...r);
     });
+  };
+
+  private editUser() {
+    alert('edit');
+  }
+
+  private deleteUser() {
+    alert('delete');
   }
 }
 </script>
